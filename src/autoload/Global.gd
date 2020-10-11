b29a1ead0ccf1590b617766c538f4bdc65c3baa6
EasternMouse <31562232+EasternMouse@users.noreply.github.com>
2020-10-11 16:10:12 +0400

    Improved fullscreen mode

--- src/autoload/Global.gd
+++ src/autoload/Global.gd
@@ -8,4 +8,8 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("fullscreen"):
 		OS.window_fullscreen = not OS.window_fullscreen
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_HIDDEN:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 		get_tree().set_input_as_handled()
